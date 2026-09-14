522f89723a9f5967f85d31271ea6d989a23e4e25
Guilment Claire <claire.guilment@recia.com>
2026-09-14 14:12:31 +0200

    fix: bind UAI replacement config as maps instead of list + PostConstruct

--- src/main/java/fr/recia/pronote/pronoteapi/config/bean/AppConfProperties.java
+++ src/main/java/fr/recia/pronote/pronoteapi/config/bean/AppConfProperties.java
@@ -1,87 +1,26 @@
-/*
- * Copyright © ${project.inceptionYear} GIP-RECIA (https://www.recia.fr/)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package fr.recia.pronote.pronoteapi.config.bean;
 
-
 import jakarta.annotation.PostConstruct;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
 
 @ConfigurationProperties(prefix = "app.conf")
 @Data
 @Validated
 @Slf4j
-@NoArgsConstructor
 public class AppConfProperties {
 
+    Map<String, String> uaiReplacementMapRequest = new HashMap<>();
 
-    @Getter(AccessLevel.NONE)
-    List<UaiToName> uaiReplacementListRequest;
-
-    @Getter(AccessLevel.NONE)
-    List<UaiToName> uaiReplacementListProxyTicketFor;
-
-    Map<String, String> uaiReplacementMapRequest;
-
-    Map<String, String> uaiReplacementMapProxyTicketFor;
-
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Data
-    static
-    class UaiToName {
-
-        String uai;
-        String name;
-    }
+    Map<String, String> uaiReplacementMapProxyTicketFor = new HashMap<>();
 
     @PostConstruct
-    public void setupAndDebug() {
-
-        uaiReplacementMapRequest = new HashMap<>();
-        for(UaiToName uaiToName : uaiReplacementListRequest){
-            uaiReplacementMapRequest.put(uaiToName.uai, uaiToName.name);
-        }
-
-        uaiReplacementMapProxyTicketFor = new HashMap<>();
-        for(UaiToName uaiToName : uaiReplacementListProxyTicketFor){
-            uaiReplacementMapProxyTicketFor.put(uaiToName.uai, uaiToName.name);
-        }
-
+    void init() {
         log.debug("AppConfProperties {}", this);
-    }
-
-    @Override
-    public String toString() {
-        return "AppConfProperties{" +
-                "uaiReplacementListRequest=" + uaiReplacementListRequest +
-                ", uaiReplacementListProxyTicketFor=" + uaiReplacementListProxyTicketFor +
-                ", uaiReplacementMapRequest=" + uaiReplacementMapRequest +
-                ", uaiReplacementMapProxyTicketFor=" + uaiReplacementMapProxyTicketFor +
-                '}';
     }
 }
