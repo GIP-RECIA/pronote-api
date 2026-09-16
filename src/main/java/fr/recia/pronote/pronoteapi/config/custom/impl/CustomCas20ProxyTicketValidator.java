1cb299d73ca540a1fd754ef3b3e2370223e7be26
Guilment Claire <claire.guilment@recia.com>
2026-09-16 14:20:38 +0200

    chore: set project inceptionYear so license headers resolve the real year

--- src/main/java/fr/recia/pronote/pronoteapi/config/custom/impl/CustomCas20ProxyTicketValidator.java
+++ src/main/java/fr/recia/pronote/pronoteapi/config/custom/impl/CustomCas20ProxyTicketValidator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © ${project.inceptionYear} GIP-RECIA (https://www.recia.fr/)
+ * Copyright © 2026 GIP-RECIA (https://www.recia.fr/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package fr.recia.pronote.pronoteapi.config.custom.impl;
 
 import fr.recia.pronote.pronoteapi.config.bean.CasProperties;
--- src/main/java/fr/recia/pronote/pronoteapi/model/viescolaire/PassageInfirmerie.java
+++ src/main/java/fr/recia/pronote/pronoteapi/model/viescolaire/PassageInfirmerie.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © ${project.inceptionYear} GIP-RECIA (https://www.recia.fr/)
+ * Copyright © 2026 GIP-RECIA (https://www.recia.fr/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
