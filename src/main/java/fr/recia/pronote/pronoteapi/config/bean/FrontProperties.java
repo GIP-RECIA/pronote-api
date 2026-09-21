9744075de3de5f606c3e4edd0cc34d9f9c5aca5a
Guilment Claire <claire.guilment@recia.com>
2026-09-21 09:16:16 +0200

    feat(backend): expose /api/config endpoint and configure CAS/Redis session

---
+++ src/main/java/fr/recia/pronote/pronoteapi/config/bean/FrontProperties.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2026 GIP-RECIA (https://www.recia.fr/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.recia.pronote.pronoteapi.config.bean;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Map;
+
+@Data
+@ConfigurationProperties(prefix = "app.front")
+public class FrontProperties {
+
+    private ExtendedUportalProperties extendedUportal;
+
+    @Data
+    public static class ExtendedUportalProperties {
+
+        private ComponentProperties header;
+        private ComponentProperties footer;
+
+        @Data
+        public static class ComponentProperties {
+            private String componentPath;
+            private Map<String, String> props;
+        }
+    }
+}
